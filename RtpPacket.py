b30804ccee1d1ad856b807860622be79b3d355f5
LongChampion <ThePalazin@Gmail.com>
2020-11-05 16:06:40 +0700

    Adjust RptPacket.py

--- RtpPacket.py
+++ RtpPacket.py
@@ -10,16 +10,11 @@ class RtpPacket:
     def __init__(self):
         pass
 
-    # --------------
-    # TO COMPLETE
-    # --------------
-    # Fill in the input arguments if needed
     def encode(self, V, P, X, CC, seqNum, M, PT, SSRC, payload):
         """Encode the RTP packet with header fields and payload."""
+        # Build header
         timestamp = int(time())
         header = bytearray(HEADER_SIZE)
-        # Fill the header bytearray with RTP header fields
-        # ...
         header[0] = header[0] | V << 6
         header[0] = header[0] | P << 5
         header[0] = header[0] | X << 4
@@ -37,10 +32,8 @@ class RtpPacket:
         header[10] = (SSRC >> 8) & 0xFF
         header[11] = SSRC & 0xFF
 
+        # Save header and payload
         self.header = header
-
-        # Get the payload
-        # ...
         self.payload = payload
 
     def decode(self, byteStream):
@@ -54,17 +47,17 @@ class RtpPacket:
 
     def seqNum(self):
         """Return sequence (frame) number."""
-        seqNum = self.header[2] << 8 | self.header[3]
+        seqNum = (self.header[2] << 8) | self.header[3]
         return int(seqNum)
 
     def timestamp(self):
         """Return timestamp."""
-        timestamp = self.header[4] << 24 | self.header[5] << 16 | self.header[6] << 8 | self.header[7]
+        timestamp = (self.header[4] << 24) | (self.header[5] << 16) | (self.header[6] << 8) | self.header[7]
         return int(timestamp)
 
     def payloadType(self):
         """Return payload type."""
-        pt = self.header[1] & 127
+        pt = self.header[1] & 0x7F
         return int(pt)
 
     def getPayload(self):
