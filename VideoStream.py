5dcd6ace66e3d21ef727036a0a3560d2bf95969e
LongChampion <ThePalazin@Gmail.com>
2020-11-05 16:05:35 +0700

    Adjust VideoStream.py

--- VideoStream.py
+++ VideoStream.py
@@ -1,5 +1,6 @@
 class VideoStream:
     def __init__(self, filename):
+		"""Attach to media file"""
         self.filename = filename
         try:
             self.file = open(filename, 'rb')
@@ -9,13 +10,9 @@ class VideoStream:
 
     def nextFrame(self):
         """Get next frame."""
-        data = self.file.read(5)  # Get the framelength from the first 5 bits
-        if data:
-            framelength = int(data)
-
-            # Read the current frame
-            data = self.file.read(framelength)
-            self.frameNum += 1
+        framelength = int(self.file.read(5))  	# Get the framelength from the first 5 bits
+        data = self.file.read(framelength)		# Read next frame
+        self.frameNum += 1
         return data
 
     def frameNbr(self):
